2938915abeb4e0af123a2524c6021ed8371fec4f
Kaylie Sheehan <kaylie.sheehan@yahoo.com>
2021-06-08 21:08:15 -0500

    added basic table rows

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,2 +1,15 @@
 // from data.js
 var tableData = data;
+// other variables
+var tableBody = d3.select('tbody');
+
+data.forEach((sighting) => {
+  var row = tableBody.append('tr')
+  var datetime = row.append('td')
+  var city = row.append('td')
+  var state = row.append('td')
+  var country = row.append('td')
+  var ahape = row.append('td')
+  var duration = row.append('td')
+  var comment = row.append('td')
+})
