b18ab141e2a915a7217734c4ac97bcf220e5b1a1
Kaylie Sheehan <kaylie.sheehan@yahoo.com>
2021-06-10 16:51:58 -0500

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -9,9 +9,9 @@ tableData.forEach((sighting) => {
   var tableBody = d3.select('tbody');
   var row = tableBody.append('tr');
   var dtrow = row.append('td').text(sighting.datetime);
-  var cityrow = row.append('td').text(sighting.city);
-  var staterow = row.append('td').text(sighting.state);
-  var countryrow = row.append('td').text(sighting.country);
+  var cityrow = row.append('td').text(sighting.city.toUpperCase());
+  var staterow = row.append('td').text(sighting.state.toUpperCase());
+  var countryrow = row.append('td').text(sighting.country.toUpperCase());
   var shaperow = row.append('td').text(sighting.shape);
   var durationrow = row.append('td').text(sighting.durationMinutes);
   var commentsrow = row.append('td').text(sighting.comments);
@@ -23,5 +23,4 @@ filterBtn.on('click', function() {
   var input = d3.select('#sighting-date');
   var inputText = input.property('value');
   var filteredData = tableData.filter(sighting => sighting.datetime === inputText);
-  console.log(filteredData);
 });
