811cf18e571cd266c6785c148afb26ab0e798046
Kaylie Sheehan <kaylie.sheehan@yahoo.com>
2021-06-10 15:06:05 -0500

    added html for the form

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,13 +3,21 @@ var tableData = data;
 // other variables
 var tableBody = d3.select('tbody');
 
-data.forEach((sighting) => {
-  var row = tableBody.append('tr')
-  var datetime = row.append('td')
-  var city = row.append('td')
-  var state = row.append('td')
-  var country = row.append('td')
-  var ahape = row.append('td')
-  var duration = row.append('td')
-  var comment = row.append('td')
+tableData.forEach((sighting) => {
+  var row = tableBody.append('tr');
+  var datetime = row.append('td');
+  var city = row.append('td');
+  var state = row.append('td');
+  var country = row.append('td');
+  var shape = row.append('td');
+  var duration = row.append('td');
+  var comments = row.append('td');
+
+  datetime.text(sighting['datetime']);
+  city.text(sighting['city']);
+  state.text(sighting['state']);
+  country.text(sighting['country']);
+  shape.text(sighting['shape']);
+  duration.text(sighting['durationMinutes']);
+  comments.text(sighting['comments']);
 })
