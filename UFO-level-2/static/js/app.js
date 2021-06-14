a01c026dbc791b1c3ce9922f9701bbad10be63c0
Kaylie Sheehan <kaylie.sheehan@yahoo.com>
2021-06-14 16:43:08 -0500

    working on multi-filter

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -25,26 +25,59 @@ const tableMaker = (data) => {
 
 tableMaker(tableData);
 
-//filter the data when the filter button is clicked
-filterBtn.on('click', function() {
+//filter data by date/time
+const filterDate = () => {
   d3.event.preventDefault();
   var input = d3.select('#sighting-date');
   var inputText = input.property('value');
-  tableData.forEach((sighting) => {
-    if (sighting.datetime === inputText) {
-      var filteredData = tableData.filter(sighting => sighting.datetime === inputText);
-    } else if (sighting.city === inputText) {
-      var filteredData = tableData.filter(sighting => sighting.city.toLowerCase() === inputText.toLowerCase());
-    } else if (sighting.state === inputText) {
-      var filteredData = tableData.filter(sighting => sighting.state.toLowerCase() === inputText.toLowerCase());
-    } else if (sighting.country === inputText) {
-      var filteredData = tableData.filter(sighting => sighting.country.toLowerCase() === inputText.toLowerCase());
-    } else if (sighting.shape === inputText) {
-      var filteredData = tableData.filter(sighting => sighting.shape === inputText.toLowerCase());
-    }
-  });
+  var filteredData = tableData.filter(sighting => sighting.datetime === inputText);
   d3.selectAll('tbody>tr').remove();
   tableMaker(filteredData);
+};
+
+//filter data by city
+const filterCity = () => {
+  d3.event.preventDefault();
+  var input = d3.select('#sighting-city');
+  var inputText = input.property('value');
+  var filteredData = tableData.filter(sighting => sighting.city.toLowerCase() === inputText.toLowerCase());
+  d3.selectAll('tbody>tr').remove();
+  tableMaker(filteredData);
+};
+
+//filter data by state
+const filterState = () => {
+  d3.event.preventDefault();
+  var input = d3.select('#sighting-state');
+  var inputText = input.property('value');
+  var filteredData = tableData.filter(sighting => sighting.state.toLowerCase() === inputText.toLowerCase());
+  d3.selectAll('tbody>tr').remove();
+  tableMaker(filteredData);
+};
+
+//filter data by country
+const filterCountry = () => {
+  d3.event.preventDefault();
+  var input = d3.select('#sighting-country');
+  var inputText = input.property('value');
+  var filteredData = tableData.filter(sighting => sighting.country.toLowerCase() === inputText.toLowerCase());
+  d3.selectAll('tbody>tr').remove();
+  tableMaker(filteredData);
+};
+
+//filter data by shape
+const filterShape = () => {
+  d3.event.preventDefault();
+  var input = d3.select('#sighting-shape');
+  var inputText = input.property('value');
+  var filteredData = tableData.filter(sighting => sighting.shape === inputText.toLowerCase());
+  d3.selectAll('tbody>tr').remove();
+  tableMaker(filteredData);
+};
+
+//filter the data when the filter button is clicked
+filterBtn.on('click', function() {
+  
 });
 
 //filter the data when enter is hit
@@ -52,19 +85,7 @@ form.on('submit', function() {
   d3.event.preventDefault();
   var input = d3.select('#sighting-date');
   var inputText = input.property('value');
-  tableData.forEach((sighting) => {
-    if (sighting.datetime === inputText) {
-      var filteredData = tableData.filter(sighting => sighting.datetime === inputText);
-    } else if (sighting.city === inputText) {
-      var filteredData = tableData.filter(sighting => sighting.city.toLowerCase() === inputText.toLowerCase());
-    } else if (sighting.state === inputText) {
-      var filteredData = tableData.filter(sighting => sighting.state.toLowerCase() === inputText.toLowerCase());
-    } else if (sighting.country === inputText) {
-      var filteredData = tableData.filter(sighting => sighting.country.toLowerCase() === inputText.toLowerCase());
-    } else if (sighting.shape === inputText) {
-      var filteredData = tableData.filter(sighting => sighting.shape === inputText);
-    }
-  });
+  var filteredData = tableData.filter(sighting => sighting.datetime === inputText);
   d3.selectAll('tbody>tr').remove();
   tableMaker(filteredData);
 });
